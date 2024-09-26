09fbc2649e3fd63caeebcb18850b56693ff3ceb2
khushal-timbaliya <khushaltimbaliya@gmail.com>
2024-09-26 14:35:37 +0530

    update

--- local storage/src/Components/Delete.jsx
+++ local storage/src/Components/Delete.jsx
@@ -6,22 +6,34 @@ export default function Delete() {
     const [city, setCity] = useState("");
 
     const [record, setRecord] = useState(null);
-    const [editIndex, setEditIndex ]=useState(null)
+    const [editIndex, setEditIndex] = useState(null)
 
     useEffect(() => {
         let data = JSON.parse(localStorage.getItem("student")) || []
         setRecord(data)
-    }, [record])
+    }, [])
 
 
 
     const handelchange = () => {
         let user = { id: Date.now(), name, subject, city }
+        let oldRecord = JSON.parse(localStorage.getItem("student")) || []
         // console.log(user);
-        record.push(user);
         // setRecord([...record, user])
-        localStorage.setItem("student", JSON.stringify(record))
 
+        if (editIndex) {
+            let singleData = record.find((item) => item.id == editIndex)
+            singleData.id = editIndex
+            singleData.name = name
+            singleData.subject = subject
+            singleData.city = city
+            localStorage.setItem("student", JSON.stringify(record))
+
+        } else {
+            oldRecord.push(user);
+            setRecord(oldRecord)
+            localStorage.setItem("student", JSON.stringify(oldRecord))
+        }
         setName("");
         setSubject("");
         setCity("");
@@ -30,11 +42,11 @@ export default function Delete() {
     const handleDelete = (id) => {
         let deleteData = record.filter((item) => item.id != id)
         setRecord(deleteData)
-        localStorage.setItem("student",JSON.stringify(deleteData))
+        localStorage.setItem("student", JSON.stringify(deleteData))
     }
 
-    const handleEdit = (id)=> { 
-        let singleDate = record.find((item)=>item.id == id)
+    const handleEdit = (id) => {
+        let singleDate = record.find((item) => item.id == id)
         setName(singleDate.name)
         setSubject(singleDate.subject)
         setCity(singleDate.city)
@@ -48,7 +60,7 @@ export default function Delete() {
             <input type="text" value={name} placeholder='enter name' onChange={(e) => setName(e.target.value)} />
             <input type="text" value={subject} placeholder='enter subject' onChange={(e) => setSubject(e.target.value)} />
             <input type="text" value={city} placeholder='enter city' onChange={(e) => setCity(e.target.value)} />
-            <button onClick={handelchange}>{editIndex? "update" : "add"}</button>
+            <button onClick={handelchange}>{editIndex ? "update" : "add"}</button>
 
             <table border='1' width='50%'>
                 <thead>
@@ -69,8 +81,8 @@ export default function Delete() {
                                     <td>{e.name}</td>
                                     <td>{e.subject}</td>
                                     <td>{e.city}</td>
-                                    <td><button onClick={()=>handleEdit(e.id)}>edit</button></td>
-                                    <td><button onClick={()=>handleDelete(e.id)}>delete</button></td>
+                                    <td><button onClick={() => handleEdit(e.id)}>edit</button></td>
+                                    <td><button onClick={() => handleDelete(e.id)}>delete</button></td>
                                 </tr>
                             })
                             :
